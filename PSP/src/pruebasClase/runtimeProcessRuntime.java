0271ba7580ab619fc85d4b9a28bc5423876f736c
pablo.rojas <pablo.rojas@WKSALMPBB907.u-tad.es>
2016-10-05 14:05:24 +0200

    Root Commit, Practicas de la asignatura de Programación de Servicios y Procesos

---
+++ PSP/src/pruebasClase/runtimeProcessRuntime.java
@@ -0,0 +1,25 @@
+package pruebasClase;
+
+import java.io.IOException;
+
+public class runtimeProcessRuntime {
+	public static void main(String[] args) {
+		if (args.length <= 0) {
+			System.err.println("Se necesita un programa a ejecutar");
+			System.exit(-1);
+		}
+		Runtime runtime = Runtime.getRuntime();
+		try {
+			Process process = runtime.exec(args);
+			process.waitFor();
+//			process.destroy();
+			System.out.println("Fin");
+		} catch (IOException ex) {
+			System.err.println("Excepción de E/S!!");
+			System.exit(-1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
